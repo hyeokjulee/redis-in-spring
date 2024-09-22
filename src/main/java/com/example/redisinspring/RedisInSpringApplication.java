c351969532de4f36398c0dbb053738e7f26e66ad
HyeokJu Lee <dlhjwwe@naver.com>
2024-09-22 15:16:31 +0900

    EC2, RDS 세팅

--- src/main/java/com/example/redisinspring/RedisInSpringApplication.java
+++ src/main/java/com/example/redisinspring/RedisInSpringApplication.java
@@ -2,12 +2,18 @@ package com.example.redisinspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class RedisInSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RedisInSpringApplication.class, args);
-	}
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+			+ "classpath:application.yml,"
+			+ "/app/config/bookitaka/real-application2.yml";
 
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(RedisInSpringApplication.class)
+				.properties(APPLICATION_LOCATIONS)
+				.run(args);
+	}
 }
