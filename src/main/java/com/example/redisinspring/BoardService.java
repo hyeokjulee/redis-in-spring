3009ffc5a7a28006fae09a538184f0cfed211807
HyeokJu Lee <dlhjwwe@naver.com>
2024-09-22 11:36:46 +0900

    Redis 세팅 추가

--- src/main/java/com/example/redisinspring/BoardService.java
+++ src/main/java/com/example/redisinspring/BoardService.java
@@ -1,5 +1,6 @@
 package com.example.redisinspring;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -15,6 +16,7 @@ public class BoardService {
         this.boardRepository = boardRepository;
     }
 
+    @Cacheable(cacheNames = "getBoards", key = "'boards:page:' + #page + ':size:' + #size", cacheManager = "BoardCacheManager")
     public List<Board> getBoards(int page, int size) {
         Pageable pageable = PageRequest.of(page - 1, size);
         Page<Board> pageOfBoards = boardRepository.findAllByOrderByCreatedAtDesc(pageable);
